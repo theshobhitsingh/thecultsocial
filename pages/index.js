685bbba090024305d1f5372f9cd59c872edfdae5
Shobhit Singh <theshobhitt@gmail.com>
2024-11-28 15:28:04 +0530

    Add files via upload
    The cult social

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import { LampDemo } from "@/components/ui/lamp";
+import { TypewriterEffectDemo } from "@/components/ui/TypewriterEffectWrapper";
+
+const HomePage = () => {
+  return (
+    <div className="home-container">
+      <LampDemo />
+      <TypewriterEffectDemo/>
+    </div>
+  );
+};
+
+export default HomePage;
+
